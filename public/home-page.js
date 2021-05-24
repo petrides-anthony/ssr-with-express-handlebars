2baf1cc0f7f526403f9360d07253155e719bc2ec
Anthony Petrides <anthony.petrides@thetrainline.com>
2021-05-24 08:14:54 +0100

    add babelrc config file

---
+++ public/home-page.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/client/home-page.js":
+/*!*********************************!*\
+  !*** ./src/client/home-page.js ***!
+  \*********************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/anthonyp/git-personal/ssr-with-express-handlebars/src/client/home-page.js: Support for the experimental syntax 'jsx' isn't currently enabled (6:5):\\n\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m \\u001b[33mReactDOM\\u001b[39m\\u001b[33m.\\u001b[39mhydrate(\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 6 |\\u001b[39m     \\u001b[33m<\\u001b[39m\\u001b[33mHomePage\\u001b[39m headerMessage\\u001b[33m=\\u001b[39m{window\\u001b[33m.\\u001b[39m__STATE__\\u001b[33m.\\u001b[39mheaderMessage} \\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33m,\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m     \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 7 |\\u001b[39m   document\\u001b[33m.\\u001b[39mgetElementById(\\u001b[32m'root'\\u001b[39m)\\u001b[0m\\n\\u001b[0m \\u001b[90m 8 |\\u001b[39m )\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:810:17)\\n    at Parser.raiseWithData (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:803:17)\\n    at Parser.expectOnePlugin (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:9982:18)\\n    at Parser.parseExprAtom (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:11367:22)\\n    at Parser.parseExprSubscripts (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:10941:23)\\n    at Parser.parseUpdate (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:10921:21)\\n    at Parser.parseMaybeUnary (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:10899:23)\\n    at Parser.parseExprOps (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:10756:23)\\n    at Parser.parseMaybeConditional (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:10730:23)\\n    at Parser.parseMaybeAssign (/Users/anthonyp/git-personal/ssr-with-express-handlebars/node_modules/@babel/parser/lib/index.js:10693:21)\");\n\n//# sourceURL=webpack://express-handlebars-example-1/./src/client/home-page.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/client/home-page.js"]();
+/******/ 	
+/******/ })()
+;
